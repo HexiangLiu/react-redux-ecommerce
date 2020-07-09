3363e8bdec3356d7f0b12a937724c39c692d3a00
Henry Liu <hl3184@columbia.edu>
2020-07-09 19:19:31 -0400

    error page complete

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './Error.scss';
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <section>
+      <div className="error">
+        <h1 className="error__heading">
+          The page you're looking for can't be found.
+        </h1>
+        <Link to="/" className="btn btn-primary">
+          back to home
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default Error;
