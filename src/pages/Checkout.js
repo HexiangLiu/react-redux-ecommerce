c16d79f9e71813bc544e0bb66d80b077c8c37ae8
Henry Liu <hl3184@columbia.edu>
2020-07-16 01:40:08 -0400

    checkout page basic structure complete

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -3,7 +3,11 @@ import { connect } from 'react-redux';
 
 import EmptyCart from '../components/EmptyCart';
 
+import './Checkout.scss';
+
 const Checkout = ({ cart, alert, total }) => {
+  const [name, setName] = useState('');
+
   if (cart.length === 0) {
     return <EmptyCart />;
   }
@@ -11,10 +15,44 @@ const Checkout = ({ cart, alert, total }) => {
   return (
     <section>
       <h2 className="heading--1">checkout</h2>
-      <form>
-        <h3>
+      <form className="form">
+        <h3 className="heading--4">
           order total : <span>{total}</span>
         </h3>
+        <div className="form-control">
+          <input
+            className="form-control__input"
+            type="text"
+            id="name"
+            value={name}
+            placeholder="Enter your name"
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+          <label htmlFor="name" className=" form-control__label">
+            Enter your name
+          </label>
+        </div>
+        <div className="instructions">
+          <label htmlFor="card-element">Credit or Debit Cart</label>
+          <p>
+            Test using this credit card : <span>4242 4242 4242 4242</span>
+            <br />
+            enter any 5 digits for the zip code
+            <br />
+            enter any 3 digits for the CVC
+          </p>
+        </div>
+        <div className="stripe">
+          {!name && (
+            <p className="checkout__alert">please fill out name field</p>
+          )}
+          {alert.show === false && (
+            <button className="btn btn-primary form-control__btn">
+              submit
+            </button>
+          )}
+        </div>
       </form>
     </section>
   );
