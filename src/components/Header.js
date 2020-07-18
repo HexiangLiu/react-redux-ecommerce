fe2dbf731b34f8ce9d983de7f8bfbc15f4eb0b0d
Henry Liu <hl3184@columbia.edu>
2020-07-18 15:17:01 -0400

    fancy responsive navbar complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { GiPineapple } from 'react-icons/gi';
 import { FaAlignJustify } from 'react-icons/fa';
 import { connect } from 'react-redux';
@@ -9,72 +9,77 @@ import './Header.scss';
 
 const Header = ({ count, user, clearCart, logout }) => {
   const [show, setShow] = useState(false);
+  const history = useHistory();
 
   return (
     <header className="header">
-      <button
-        type="button"
-        className="navbar__toggle"
-        onClick={() => setShow(!show)}
+      <nav
+        className="navbar"
+        onClick={(e) => {
+          if (e.target.matches('a') || e.target.className === 'logout__btn') {
+            setShow(false);
+          }
+        }}
       >
-        <FaAlignJustify />
-      </button>
-      <nav className="navbar">
-        <ul className="navbar__items">
+        <button
+          type="button"
+          className="navbar__toggle"
+          onClick={() => setShow(!show)}
+        >
+          <FaAlignJustify />
+        </button>
+        <Link className="header__icon navbar__link" to="/">
+          <GiPineapple />
+        </Link>
+        <ul className={show ? 'navbar__target show' : 'navbar__target'}>
           <li className="navbar__item">
-            <Link className="header__icon navbar__link" to="/">
-              <GiPineapple />
+            <Link className="navbar__link" to="/">
+              Home
             </Link>
           </li>
-          <div className={show ? 'navbar__pages show' : 'navbar__pages'}>
-            <li className="navbar__item">
-              <Link className="navbar__link" to="/">
-                Home
-              </Link>
-            </li>
-            <li className="navbar__item">
-              <Link className="navbar__link" to="/about">
-                About
-              </Link>
-            </li>
-            <li className="navbar__item">
-              <Link className="navbar__link" to="/products">
-                Product
-              </Link>
-            </li>
+          <li className="navbar__item">
+            <Link className="navbar__link" to="/about">
+              About
+            </Link>
+          </li>
+          <li className="navbar__item">
+            <Link className="navbar__link" to="/products">
+              Product
+            </Link>
+          </li>
 
-            {/* only display the checkout nav when the user successfully signed in */}
-            {user && (
-              <li className="navbar__item">
-                <Link className="navbar__link" to="/checkout">
-                  Checkout
-                </Link>
-              </li>
-            )}
-            {/* display login or log out nav depending on user's status */}
+          {/* only display the checkout nav when the user successfully signed in */}
+          {user && (
             <li className="navbar__item">
-              {user ? (
-                <button
-                  onClick={() => {
-                    clearCart();
-                    logout();
-                  }}
-                >
-                  Log Out
-                </button>
-              ) : (
-                <Link className="navbar__link" to="/login">
-                  Login
-                </Link>
-              )}
-            </li>
-            <li className="navbar__item navbar__item--cart">
-              <Link className="navbar__link" to="/cart">
-                Cart <span className="navbar__cart">{count}</span>
+              <Link className="navbar__link" to="/checkout">
+                Checkout
               </Link>
             </li>
-          </div>
+          )}
+          {/* display login or log out nav depending on user's status */}
+          <li className="navbar__item">
+            {user ? (
+              <button
+                className="logout__btn"
+                onClick={() => {
+                  clearCart();
+                  logout();
+                  history.push('/');
+                }}
+              >
+                Log out
+              </button>
+            ) : (
+              <Link className="navbar__link" to="/login">
+                Login
+              </Link>
+            )}
+          </li>
         </ul>
+
+        <Link className="navbar__link" to="/cart">
+          Cart <span className="navbar__cart">{count}</span>
+        </Link>
       </nav>
     </header>
   );
